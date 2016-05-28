12b1541771a34b5453852ccb6333cf618e061cf0
bluefuton <chris@bluefuton.com>
2016-05-28 10:53:21 -0600

    Load a new recommendation when user interacts with a card
    - store interacted cards in the Redux tree
    - get child recommendation ID for card where available
    - only fetch 1 recommendation from API in recordRecommendationInteraction()
    - don't accept duplicate recommendations
    - add test for recommendation insertion order

--- client/reader/start/card.jsx
+++ client/reader/start/card.jsx
@@ -1,6 +1,7 @@
 // External dependencies
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import debugModule from 'debug';
 import get from 'lodash/get';
 import classnames from 'classnames';
@@ -10,37 +11,54 @@ import Card from 'components/card';
 import StartPostPreview from './post-preview';
 import StartCardHeader from './card-header';
 import StartCardFooter from './card-footer';
+import { recordRecommendationInteraction } from 'state/reader/start/actions';
 import { getRecommendationById } from 'state/reader/start/selectors';
 import { getSite } from 'state/reader/sites/selectors';
 
 const debug = debugModule( 'calypso:reader:start' ); //eslint-disable-line no-unused-vars
 
-const StartCard = ( { site, siteId, postId } ) => {
-	const headerImage = site.header_image;
+const StartCard = React.createClass( {
+	onCardInteraction() {
+		this.props.recordRecommendationInteraction(
+			this.props.recommendationId,
+			this.props.recommendation.recommended_site_ID,
+			this.props.recommendation.recommended_post_ID
+		);
+	},
 
-	let heroStyle;
-	if ( headerImage ) {
-		heroStyle = {
-			backgroundImage: `url("${ headerImage.url }")`
-		};
-	}
+	render() {
+		const { site, siteId, postId } = this.props;
 
-	const cardClasses = classnames(
-		'reader-start-card',
-		{
-			'has-post-preview': ( postId > 0 )
+		if ( ! site ) {
+			return null;
 		}
-	);
 
-	return (
-		<Card className={ cardClasses }>
-			<div className="reader-start-card__hero" style={ heroStyle }></div>
-			<StartCardHeader siteId={ siteId } />
-			{ postId > 0 && <StartPostPreview siteId={ siteId } postId={ postId } /> }
-			<StartCardFooter siteId={ siteId } />
-		</Card>
-	);
-};
+		const headerImage = site.header_image;
+
+		let heroStyle;
+		if ( headerImage ) {
+			heroStyle = {
+				backgroundImage: `url("${ headerImage.url }")`
+			};
+		}
+
+		const cardClasses = classnames(
+			'reader-start-card',
+			{
+				'has-post-preview': ( postId > 0 )
+			}
+		);
+
+		return (
+			<Card className={ cardClasses } onClick={ this.onCardInteraction }>
+				<div className="reader-start-card__hero" style={ heroStyle }></div>
+				<StartCardHeader siteId={ siteId } />
+				{ postId > 0 && <StartPostPreview siteId={ siteId } postId={ postId } /> }
+				<StartCardFooter siteId={ siteId } />
+			</Card>
+		);
+	}
+} );
 
 StartCard.propTypes = {
 	recommendationId: React.PropTypes.number.isRequired
@@ -54,9 +72,13 @@ export default connect(
 		const site = getSite( state, siteId );
 
 		return {
+			recommendation,
 			siteId,
 			postId,
 			site
 		};
-	}
+	},
+	( dispatch ) => bindActionCreators( {
+		recordRecommendationInteraction
+	}, dispatch )
 )( StartCard );
--- client/state/reader/start/reducer.js
+++ client/state/reader/start/reducer.js
@@ -2,7 +2,12 @@
  * External dependencies
  */
 import { combineReducers } from 'redux';
-import keyBy from 'lodash/keyBy';
+import union from 'lodash/union';
+import findIndex from 'lodash/findIndex';
+import forEach from 'lodash/forEach';
+import filter from 'lodash/filter';
+import has from 'lodash/has';
+import debugModule from 'debug';
 
 /**
  * Internal dependencies
@@ -12,6 +17,7 @@ import {
 	READER_START_RECOMMENDATIONS_REQUEST,
 	READER_START_RECOMMENDATIONS_REQUEST_SUCCESS,
 	READER_START_RECOMMENDATIONS_REQUEST_FAILURE,
+	READER_START_RECOMMENDATION_INTERACTION,
 	SERIALIZE,
 	DESERIALIZE,
 } from 'state/action-types';
@@ -19,21 +25,56 @@ import { itemsSchema } from './schema';
 import { isValidStateWithSchema } from 'state/utils';
 
 /**
+ * Module variables
+ */
+const debug = debugModule( 'calypso:redux:reader-start-recommendations' );
+
+/**
  * Tracks all known list objects, indexed by list ID.
  *
- * @param  {Object} state  Current state
+ * @param  {Array} state  Current state
  * @param  {Object} action Action payload
- * @return {Object}        Updated state
+ * @return {Array}        Updated state
  */
-export function items( state = {}, action ) {
+export function items( state = [], action ) {
 	switch ( action.type ) {
 		case READER_START_RECOMMENDATIONS_RECEIVE:
-			return Object.assign( {}, state, keyBy( action.recommendations, 'ID' ) );
+			let updatedRecommendations = state;
+
+			// Filter out any recommendations we already have
+			const newRecommendations = filter( action.recommendations, ( incomingRecommendation ) => {
+				if ( findIndex( state, ( existingRecommendation ) => {
+					return existingRecommendation.ID === incomingRecommendation.ID;
+				} ) >= 0 ) {
+					return false;
+				}
+				return true;
+			} );
+
+			const totalRecommendationsCount = state.length + newRecommendations.length;
+
+			forEach( newRecommendations, ( recommendation ) => {
+				// We want to insert the new recommendation immediately after its parent, if there is one
+				let parentPosition = findIndex( state, ( item ) => {
+					if ( ! has( item, 'recommended_site_ID' ) ) {
+						return false;
+					}
+					return item.recommended_site_ID === recommendation.origin_site_ID && item.recommended_post_ID === recommendation.origin_post_ID;
+				} );
+
+				if ( parentPosition < 0 ) {
+					parentPosition = totalRecommendationsCount - 1;
+				}
+
+				updatedRecommendations = insertItemAtArrayPosition( updatedRecommendations, recommendation, parentPosition );
+			} );
+			return updatedRecommendations;
+
 		case SERIALIZE:
 			return state;
 		case DESERIALIZE:
 			if ( ! isValidStateWithSchema( state, itemsSchema ) ) {
-				return {};
+				return [];
 			}
 			return state;
 	}
@@ -62,7 +103,27 @@ export function isRequestingRecommendations( state = false, action ) {
 	return state;
 }
 
+export function recommendationsInteractedWith( state = [], action ) {
+	switch ( action.type ) {
+		case READER_START_RECOMMENDATION_INTERACTION:
+			return union( state, [ action.recommendationId ] );
+
+		case SERIALIZE:
+		case DESERIALIZE:
+			return state;
+	}
+
+	return state;
+}
+
+function insertItemAtArrayPosition( array, item, position ) {
+	const beforeSlice = array.slice( 0, position );
+	const afterSlice = array.slice( position );
+	return beforeSlice.concat( item, afterSlice );
+}
+
 export default combineReducers( {
 	items,
-	isRequestingRecommendations
+	isRequestingRecommendations,
+	recommendationsInteractedWith
 } );
